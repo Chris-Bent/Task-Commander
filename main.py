e8e5a86fe11c5e0808305256b67910b53b3fadf2
Chris-Bent <chriswjbent@gmail.com>
2024-12-06 18:32:05 +0000

    Understanding commands

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ def main():
     subparsers = parser.add_subparsers(dest="command", help="Available Commands")
 
     # Subparser for 'add' command
+    add_parser = subparsers.add_parser("add", help="Add a new task")
+    # Default positional argument for name
+    add_parser.add_argument("name", required=True, help="Name of the task")
+    # Additional positional arguments for added detail on tasks
+    add_parser.add_argument("--d", required=False, help="Task Description")
+    
+    args = parser.parse_args()
 
 if __name__ == "__main__":
     main()
