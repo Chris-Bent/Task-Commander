4ce1d9cbd51b81ada6683c8143b39b95358ba3bf
Chris-Bent <chriswjbent@gmail.com>
2024-12-08 09:57:13 +0000

    no clue what im at

--- cmdr.py
+++ cmdr.py
@@ -4,18 +4,19 @@ import json
 import datetime 
 
 def main():
-    task = input("What is ur task?") # should be read from an argparse command 
 
     def addTask(task):
-    
-        if os.path.exists("cmdrtasks.json"):
-            print(task, "Exists")
-        else:
-            with open("cmdrtasks.json", "w") as f:
-                print("JSON File Created")
-        
+        return os.path.exists("cmdrtasks.json")
 
-    addTask(task)
+        if addtask("cmdrtasks.json"):
+            pass
+        else:
+            data = {
+                "task": input("Task: ")
+            }
+
+            with open("cmdrtasks.json", "w") as fileOut: # w for write r for read
+                json.dump(data, fileOut)
 
 main()
 
